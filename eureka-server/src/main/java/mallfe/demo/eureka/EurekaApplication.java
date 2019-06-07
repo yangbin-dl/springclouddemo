8ca1b506f9f37b774d89893033f997b97be2ea70
yangbin-dl <yangbin-jl@163.com>
2019-06-07 15:28:04 +0800

    mac

--- eureka-server/src/main/java/mallfe/demo/eureka/EurekaApplication.java
+++ eureka-server/src/main/java/mallfe/demo/eureka/EurekaApplication.java
@@ -1,6 +1,9 @@
 package mallfe.demo.eureka;
 
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * 描述
@@ -9,7 +12,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 2019/06/07
  */
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaApplication {
-
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class);
+    }
 }
