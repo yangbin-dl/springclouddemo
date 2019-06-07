c44ad37237193f09d6d0bea07cf63297f25f7e32
yangbin-dl <yangbin-jl@163.com>
2019-06-07 10:58:14 +0800

    mac

---
+++ eureka-server/src/main/java/mallfe/demo/eureka/EurekaApplication.java
@@ -0,0 +1,15 @@
+package mallfe.demo.eureka;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 描述
+ *
+ * @author Yangbin
+ * @since 2019/06/07
+ */
+
+@SpringBootApplication
+public class EurekaApplication {
+
+}
